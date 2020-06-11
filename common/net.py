c9430f7af34d17fa782974cd8344c78508e4f7c8
WillerFeng <j514681085@icloud.com>
2020-06-11 16:07:15 +0800

    complete net

--- common/net.py
+++ common/net.py
@@ -6,92 +6,117 @@ import torch.nn.functional as F
 
 class Atari_ConvNet(nn.Module):
     
-    def __init__(self, in_channels:int, action_space:int, type:str):
+    def __init__(self, in_channels:int, action_space:int, net_type:str):
         super(Atari_ConvNet, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32         , 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64         , 64, kernel_size=3, stride=1)
         
-        self.fc1   = nn.Linear(7 * 7 * 64, 512)
-        self.fc2   = nn.Linear(512, num_actions)
+        self.fc1 = nn.Linear(7 * 7 * 64, 512)
+        self.fc2 = nn.Linear(512, action_space)
         
-        if type == 'DQN':
+        if   net_type == 'value':
             self.last_active = nn.ReLU()
-        elif type == 'Policy':
-            self.last_active = nn.softmax()
+        elif net_type == 'policy':
+            self.last_active = nn.softmax(dim=1)
+        else:
+            raise ValueError("Undefined net type")
         
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = self.last_active(self.fc2(x))
-        return x
+        
+        action = F.relu(self.fc1(x))
+        action = self.last_active(self.fc2(action))
+        return action, value
         
         
         
 class Atari_Dueling_ConvNet(nn.Module):
     
-    def __init__(self, in_channels:int, action_space:int, type:str):
-        super(Atari_ConvNet, self).__init__()
+    def __init__(self, in_channels:int, action_space:int, net_type:str):
+        super(Atari_Dueling_ConvNet, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32         , 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64         , 64, kernel_size=3, stride=1)
                    
-        self.fc1   = nn.Linear(7 * 7 * 64, 512)
-        self.fc2   = nn.Linear(512, num_actions)
+        self.fc_action1 = nn.Linear(7 * 7 * 64, 512)
+        self.fc_action2 = nn.Linear(512, action_space)
         
-        if type == 'DQN':
+        self.fc_value1  = nn.Linear(7 * 7 * 64, 512)
+        self.fc_value2  = nn.Linear(512, 1)
+        
+        if   net_type == 'value':
             self.last_active = nn.ReLU()
-                   
-        elif type == 'Policy':
-            self.last_active = nn.softmax()
+        elif net_type == 'policy':
+            self.last_active = nn.softmax(dim=1)
+        else:
+            raise ValueError("Undefined net type")
     
         
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
-        x = F.relu(self.fc4(x.view(x.size(0), -1)))
-        return self.fc5(x)
+        x = x.view(x.size(0), -1)
+        
+        action = F.relu(self.fc_action1(x))
+        action = self.last_active(self.fc_action2(action))
+        
+        value = F.relu(self.fc_value1(x))
+        value = self.fc_value2(value)
+        return action, value
         
         
         
 class FullyNet(nn.Module):
     
-    def __init__(self, in_channels:int, action_space:int, type:str, hidden_size:int=256):        
-        super(FullNet, self).__init__()
-        self.fc1 = nn.Linear(inchannels , hidden_size)
+    def __init__(self, state_space:int, action_space:int, net_type:str, hidden_size:int=256):        
+        super(FullyNet, self).__init__()
+        self.fc1 = nn.Linear(state_space, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, action_space)
         
         
-        if type == 'DQN':
+        if   net_type == 'value':
             self.last_active = nn.ReLU()
-                   
-        elif type == 'Policy':
-            self.last_active = nn.softmax()
+        elif net_type == 'policy':
+            self.last_active = nn.softmax(dim=1)
+        else:
+            raise ValueError("Undefined net type")
             
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.last_active(x)
-        return x
+        action = self.last_active(x)
+        return action
         
         
         
 class Dueling_FullyNet(nn.Module):
     
-    def __init__(self, in_channels:int, action_space:int, type:str, hidden_size:int=256):       
-        super(FullNet, self).__init__()
-        self.fc1 = nn.Linear(inchannels , hidden_size)
+    def __init__(self, state_space:int, action_space:int, net_type:str, hidden_size:int=256):       
+        super(Dueling_FullyNet, self).__init__()
+        self.fc1 = nn.Linear(state_space, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, action_space)
         
+        self.fc_value  = nn.Linear(hidden_size, 1)
+        self.fc_action = nn.Linear(hidden_size, action_space)
+        
+        if   net_type == 'value':
+            self.last_active = nn.ReLU()
+        elif net_type == 'policy':
+            self.last_active = nn.Softmax(dim=1)
+        else:
+            raise ValueError("Undefined net type")
+            
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.last_active(x)
-        return x
+        
+        action = self.last_active(self.fc_action(x))
+        value  = self.fc_value(x)
+        return action, value
                    
